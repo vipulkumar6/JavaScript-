95e466cda26457678644e3174214b87b3928faa8
Vipul Kumar <92398016+itsvipulkumar@users.noreply.github.com>
2023-02-24 00:14:14 +0530

    map

---
+++ 08.js
@@ -0,0 +1,18 @@
+function myFun() {
+    console.log("Hello JS  ");
+}
+
+// for (let i = 0; i < 5; i++) {
+//     myFun();
+//     console.log("");
+// }
+
+const myFun2 = (a, b) => {
+    return a + b;
+}
+
+// let a = prompt("Enter value of a : ");
+// let b = prompt("Enter value of b : ");
+
+let sum = myFun2(10, 20);
+console.log(sum)
---
+++ map.js
@@ -0,0 +1,22 @@
+const CardData = [
+    {
+        id: 1,
+        Name: "Vipul",
+    },
+    {
+        id: 2,
+        Name: "Aman",
+    },
+    {
+        id: 3,
+        Name: "Shuaib",
+    }
+]
+
+
+const newArr = CardData.map((val, i) => {
+    return val;
+})
+
+console.log(newArr);
+console.log("Hello")
